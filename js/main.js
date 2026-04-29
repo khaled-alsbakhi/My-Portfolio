efb6b06d984c1801db6ced80a93e256e2e6bafaf
KhaledAyman <bvn.2002@hotmail.com>
2026-04-29 21:13:35 +0300

    Added The Project Counter Function

--- js/main.js
+++ js/main.js
@@ -63,6 +63,34 @@ const barObs = new IntersectionObserver(
 
 document.querySelectorAll(".skill-card").forEach((el) => barObs.observe(el));
 
+//Increase counter on scroll
+let nums = document.querySelectorAll(".stat .stat-num");
+let section = document.querySelector(".stat-row");
+let statStarted = false; //function started ? No
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop) {
+    if (!statStarted) {
+      nums.forEach((num) => startCount(num));
+    }
+    statStarted = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let showPlus = el.dataset.plus;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+      if (showPlus === "true") {
+        el.textContent = "+" + el.textContent;
+      }
+    }
+  }, 1000 / goal);
+}
+
 // Active nav
 const sections = document.querySelectorAll("section[id]");
 window.addEventListener("scroll", () => {
